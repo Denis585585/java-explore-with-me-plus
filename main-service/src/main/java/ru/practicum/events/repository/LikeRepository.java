25af498faea9abae7d4f46e53661c3c73c66c74d
Denis585585 <Vegan585@yandex.ru>
2025-05-09 21:12:42 +0300

    Поправил репозиторий

--- main-service/src/main/java/ru/practicum/events/repository/LikeRepository.java
+++ main-service/src/main/java/ru/practicum/events/repository/LikeRepository.java
@@ -3,7 +3,8 @@ package ru.practicum.events.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.events.model.Like;
+import ru.practicum.events.model.LikeId;
 
 @Repository
-public interface LikeRepository extends JpaRepository<Integer, Like> {
+public interface LikeRepository extends JpaRepository<Like, LikeId> {
 }
